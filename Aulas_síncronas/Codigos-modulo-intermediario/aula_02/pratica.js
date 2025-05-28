c02a213fc682f04a76628627ed5a761c2035e50a
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2025-05-28 09:41:56 -0300

    feat: add codigo da aula 02

---
+++ Aulas_síncronas/Codigos-modulo-intermediario/aula_02/pratica.js
@@ -0,0 +1,14 @@
+let numeros = [];
+for (let i = 23; i < 54; i++) {
+  numeros.push(i);
+}
+
+numeros.forEach((element) => {
+  console.log(element);
+});
+console.log('\n-----------------')
+let nomes = ["Dino", "Baby", "Charlote"];
+
+nomes.map((nome) => {
+  console.log(nome + " da Silva Sauro");
+});
